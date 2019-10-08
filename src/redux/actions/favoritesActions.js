47f2ee9f5c115957b44964421cf62d1eb8d26362
sigfried <rssanjuan2704@gmail.com>
2019-10-08 00:01:28 -0500

    [ADD] Async actions.

--- src/redux/actions/favoritesActions.js
+++ src/redux/actions/favoritesActions.js
@@ -1,5 +1,30 @@
+import client from "../../httpClient";
+
+export const GET_ALL_FAVORITES = "GET_ALL_FAVORITES";
 export const ADD_FAVORITE = "ADD_FAVORITE";
 export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
 
 export const addFavorite = payload => ({ type: ADD_FAVORITE, payload });
 export const removeFavorite = payload => ({ type: REMOVE_FAVORITE, payload });
+
+//* Thunk example
+export const addFavoriteAsync = payload => {
+  return async dispatch => {
+    await client.post(`images/fav/${payload.id}`);
+    dispatch(addFavorite(payload));
+  };
+};
+
+export const removeFavoriteAsync = payload => {
+  return async dispatch => {
+    await client.delete(`images/fav/${payload.id}`);
+    dispatch(removeFavorite(payload));
+  };
+};
+
+export const getFavoritesAsync = () => {
+  return async dispatch => {
+    const { results } = await client.get(`images/fav/`);
+    console.log(results.data.images);
+  };
+};
