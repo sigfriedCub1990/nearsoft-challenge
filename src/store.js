a1d27e450b474cf2ca889c93db0598dc30a2221a
sigfried <rssanjuan2704@gmail.com>
2019-10-08 00:00:29 -0500

    [ADD] Thunk as middleware.

--- src/store.js
+++ src/store.js
@@ -1,7 +1,5 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { favoritesReducer } from "./redux/reducers/favoritesReducer";
 
-export const store = createStore(
-  favoritesReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export const store = createStore(favoritesReducer, applyMiddleware(thunk));
